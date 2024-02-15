99cc5bca653b8bcbfeb33644a64d71d43c80a8dd
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-02-15 18:38:25 +0700

    feat(components/common): create common component
    1. create `ShowIf` component
    2. create `For` component
    3. create `Box` component

---
+++ apps/docs/src/components/common.stories.tsx
@@ -0,0 +1,56 @@
+import {Box, Button, For, ShowIf} from "@bitwyre/ui-kit";
+import * as React from "react";
+
+const data = [
+  {name: "John", age: 24},
+  {name: "Adi", age: 21},
+  {name: "Hansen", age: 27},
+];
+
+export const Box_ = () => (
+  <Box as="header">
+    <p>
+      I rendered inside a box rendered as <code>header</code>
+    </p>
+  </Box>
+);
+
+export const For_ = () => (
+  <>
+    <p>
+      <code>For</code> Component
+    </p>
+
+    <ul className="list-disc list-inside">
+      <For each={data}>
+        {(item) => (
+          <li>
+            Hello, my name is {item.name}, and I am {item.age} Years old
+          </li>
+        )}
+      </For>
+    </ul>
+  </>
+);
+
+export const ShowIf_ = () => {
+  const [bool, setBool] = React.useState(true);
+  const toggleBool = () => setBool((prev) => !prev);
+
+  return (
+    <>
+      <p>
+        <code>ShowIf</code> Component
+      </p>
+      <Button variant="secondary" onClick={toggleBool}>
+        Toggle show
+      </Button>
+
+      <ShowIf
+        condition={bool}
+        fallback={<p>The condition is false, this is fallback, but optional</p>}>
+        <p>The condition is true</p>
+      </ShowIf>
+    </>
+  );
+};
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,5 +1,6 @@
 export * from "./button";
 export * from "./card";
+export * from "./common";
 export * from "./drawer";
 export * from "./input";
 export * from "./notify";
