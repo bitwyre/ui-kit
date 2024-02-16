379afb0750fc3065980f6798a2aa30aa248168ad
Deni Sugiarto <45736463+denisugiarto@users.noreply.github.com>
2024-02-16 11:46:15 +0700

    feat(button-component): create button component (#12)
    * feat(button-component): create button component
    
    Create button component
    
    * fix(button-component): fix icon loading in button
    
    * fix(button-component): make loading icon color from parent, and update docs for props asChild
    
    * feat(button-component): adding more variants and fix documentation about component
    
    ---------
    
    Co-authored-by: Ioofy <mynev.id@gmail.com>

--- apps/docs/src/components/button.stories.tsx
+++ apps/docs/src/components/button.stories.tsx
@@ -1,20 +1,66 @@
 import {Button} from "@bitwyre/ui-kit";
 
-const ComponentLinkButton = () => (
-  <Button type="button" variant="link">
-    This is button link
-  </Button>
+const ComponentPrimaryButton = () => (
+  <Button variant="primary">This is button Primary</Button>
 );
 
 const ComponentSecondaryButton = () => (
-  <Button type="button" variant="secondary">
-    This is button secondary
+  <Button variant="secondary">This is button secondary</Button>
+);
+const ComponentSubtleButton = () => (
+  <Button variant="subtle">This is button subtle</Button>
+);
+const ComponentOutlineButton = () => (
+  <Button variant="outline" className="text-black">
+    This is button outline
+  </Button>
+);
+const ComponentDestructiveButton = () => (
+  <Button variant="destructive">This is button destructive</Button>
+);
+const ComponentGhostButton = () => (
+  <Button variant="ghost" className="text-black hover:text-white">
+    This is button ghost
+  </Button>
+);
+const ComponentLinkButton = () => <Button variant="link">This is button link</Button>;
+const ComponentGradientButton = () => (
+  <Button variant="gradient-blue">This is button gradient</Button>
+);
+const ComponentLoadingButton = () => (
+  <Button variant="subtle" isButtonLoading={true} textButtonLoading="Loading...">
+    This is button loading
+  </Button>
+);
+const ComponentIconDefaultButton = () => (
+  <Button variant="subtle" size="icon">
+    +
+  </Button>
+);
+const ComponentIconGhostButton = () => (
+  <Button variant="ghost" size="icon" className="text-black hover:text-white">
+    +
+  </Button>
+);
+const ComponentIconCircleButton = () => (
+  <Button variant="subtle" size="iconCircle">
+    +
   </Button>
 );
 
-export const Main = () => (
-  <>
-    <ComponentLinkButton />
+export const MainComponent = () => (
+  <div className="flex gap-2 flex-wrap">
+    <ComponentPrimaryButton />
     <ComponentSecondaryButton />
-  </>
+    <ComponentSubtleButton />
+    <ComponentOutlineButton />
+    <ComponentDestructiveButton />
+    <ComponentGradientButton />
+    <ComponentGhostButton />
+    <ComponentLinkButton />
+    <ComponentLoadingButton />
+    <ComponentIconDefaultButton />
+    <ComponentIconGhostButton />
+    <ComponentIconCircleButton />
+  </div>
 );
