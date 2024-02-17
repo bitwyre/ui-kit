8ff116881a36f321d88185bfeb1fc5af50228c61
Ioofy <mynev.id@gmail.com>
2024-02-17 10:50:42 +0700

    feat(dropdown-component): creating dropdown menu component with docs
    Creating dropdown menu component with docs

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -2,6 +2,7 @@ export * from "./button";
 export * from "./card";
 export * from "./common";
 export * from "./drawer";
+export * from "./dropdown-menu";
 export * from "./input";
 export * from "./notify";
 export * from "./popover";
