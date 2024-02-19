5c3ea474c7b26966028490c97d4c31baf2ad7ef4
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-02-19 12:07:41 +0700

    docs(ui/hooks/usemediaquery.ts): add jsdoc to `useMediaQuery` hooks

--- packages/ui/src/hooks/useMediaQuery.ts
+++ packages/ui/src/hooks/useMediaQuery.ts
@@ -1,5 +1,23 @@
 import {useCallback, useSyncExternalStore} from "react";
 
+/**
+ * useMediaQuery hooks tells the app if a CSS media query matches with the current state
+ * Common use case is to tell the app if the current device is a mobile device or desktop device
+ *
+ * @example
+ * ```tsx
+ * const MyComponent = () => {
+ *  const isMobile = useMediaQuery('only screen and (max-width: 650px)')
+ *
+ *  if(isMobile) return // mobile component
+ *
+ *  return // desktop component
+ * }
+ * ```
+ *
+ * @param query the media query you want to watch
+ * @returns boolean
+ */
 export const useMediaQuery = (query: string) => {
   const subscribe = useCallback(
     (listener: () => void) => {
