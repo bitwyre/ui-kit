5d9c91f3e8573fd0bc1a79c66980d36639b51291
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-02-19 15:00:41 +0700

    refactor(docs/components/table.stories.tsx): update use case example

--- apps/docs/src/components/table.stories.tsx
+++ apps/docs/src/components/table.stories.tsx
@@ -1,4 +1,5 @@
 import {
+  For,
   Table,
   TableBody,
   TableCell,
@@ -6,32 +7,105 @@ import {
   TableHead,
   TableHeader,
   TableRow,
+  cn,
 } from "@bitwyre/ui-kit";
 
+const data = [
+  {
+    name: "John",
+    age: 24,
+    job: "Software Engineer",
+    salary: 100000,
+    favLang: "JS",
+    hobby: "Football",
+    os: "Mac",
+    keyboard: "Logitech",
+  },
+  {
+    name: "Adi",
+    age: 24,
+    job: "Software Engineer",
+    salary: 100000,
+    favLang: "JS",
+    hobby: "Football",
+    os: "Mac",
+    keyboard: "Logitech",
+  },
+  {
+    name: "Hansen",
+    age: 27,
+    job: "Software Engineer",
+    salary: 100000,
+    favLang: "JS",
+    hobby: "Football",
+    os: "Mac",
+    keyboard: "Logitech",
+  },
+  {
+    name: "Rim",
+    age: 24,
+    job: "Software Engineer",
+    salary: 100000,
+    favLang: "JS",
+    hobby: "Football",
+    os: "Mac",
+    keyboard: "Logitech",
+  },
+  {
+    name: "Rin",
+    age: 24,
+    job: "Software Engineer",
+    salary: 100000,
+    favLang: "JS",
+    hobby: "Football",
+    os: "Mac",
+    keyboard: "Logitech",
+  },
+  {
+    name: "Sana",
+    age: 24,
+    job: "Software Engineer",
+    salary: 100000,
+    favLang: "JS",
+    hobby: "Football",
+    os: "Mac",
+    keyboard: "Logitech",
+  },
+];
 export const TableComponent = () => {
   return (
-    <Table>
+    <Table className="table-auto">
       <TableHeader>
-        <TableHead>No</TableHead>
-        <TableHead>Name</TableHead>
-        <TableHead>Age</TableHead>
+        <TableRow>
+          <TableHead>NO</TableHead>
+          <For each={Object.keys(data[0])}>
+            {(key) => <TableHead>{key.toUpperCase()}</TableHead>}
+          </For>
+        </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
-          <TableCell>1</TableCell>
-          <TableCell>John</TableCell>
-          <TableCell>24</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>2</TableCell>
-          <TableCell>Adi</TableCell>
-          <TableCell>24</TableCell>
-        </TableRow>
+        <For each={data}>
+          {(item, idx) => (
+            <TableRow>
+              <TableCell>{idx + 1}</TableCell>
+              <For each={Object.values(item)}>
+                {(value, idxValue) => (
+                  <TableCell
+                    className={cn(idx === 0 && idxValue === 2 && "p-0 text-nowrap ")}>
+                    {value}
+                  </TableCell>
+                )}
+              </For>
+            </TableRow>
+          )}
+        </For>
       </TableBody>
       <TableFooter>
-        <TableHead>Total</TableHead>
-        <TableHead>24</TableHead>
-        <TableHead>48</TableHead>
+        <TableRow>
+          <TableCell colSpan={Object.keys(data[0]).length + 1} className="text-end">
+            Total {data.length}
+          </TableCell>
+        </TableRow>
       </TableFooter>
     </Table>
   );
